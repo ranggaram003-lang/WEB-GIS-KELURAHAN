55882ed210c6f6b6578c0075675a2d8d8f2f2384
ranggaram003-lang <ranggaram003@gmail.com>
2026-04-18 23:56:03 +0800

    Add files via upload

---
+++ data/PERDAGANGANDANJASA_6.js
@@ -0,0 +1,1 @@
+var json_PERDAGANGANDANJASA_6 = {"type":"FeatureCollection","name":"PERDAGANGANDANJASA_6","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Id":0.0},"geometry":{"type":"Point","coordinates":[119.695688842440546,-4.584786644854328]}}]}
